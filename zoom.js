2647f14b448a89feeec0e2037e0c975498d95b1f
Adam Thomas <thomasadam83@hotmail.com>
2022-04-01 10:28:33 -0400

    Added comments to JavaScript

--- zoom.js
+++ zoom.js
@@ -9,6 +9,7 @@ zoomTarget.addEventListener('mousemove', e => {
 
     e.preventDefault();
 
+    // Calculate the x and y position in the target
     let rect = e.currentTarget.getBoundingClientRect(); 
 
     let x = e.clientX - rect.x;
@@ -17,6 +18,7 @@ zoomTarget.addEventListener('mousemove', e => {
     x -= zoomMag.offsetWidth / 2;
     y -= zoomMag.offsetHeight / 2;
 
+    // Ensure the mag does not leave the target
     if(x < 0) x = 0;
     if(x > zoomTarget.offsetWidth - zoomMag.offsetWidth) 
         x = zoomTarget.offsetWidth - zoomMag.offsetWidth;
@@ -25,15 +27,18 @@ zoomTarget.addEventListener('mousemove', e => {
     if(y > zoomTarget.offsetHeight - zoomMag.offsetHeight) 
         y = zoomTarget.offsetHeight - zoomMag.offsetHeight;
 
+    // Reposition the mag
     zoomMag.style.left = x + 'px';
     zoomMag.style.top = y + 'px';
 
+    // Calculate the new position og the zoomed in image
     let xPercent = x / ( zoomTarget.offsetWidth - zoomMag.offsetWidth );
     let yPercent = y / ( zoomTarget.offsetHeight - zoomMag.offsetHeight );
 
     let imageX = ( zoomImage.offsetWidth - zoomViewer.offsetWidth ) * xPercent;
     let imageY = ( zoomImage.offsetHeight - zoomViewer.offsetHeight ) * yPercent;
 
+    // Reposition the image
     zoomImage.style.left = -imageX + 'px';
     zoomImage.style.top = -imageY + 'px';
 
